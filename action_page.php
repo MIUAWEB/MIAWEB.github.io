6fe6838e43df5b06eb57ac8e1bfecb54908802e3
ailtondiaz <100169074+ailtondiaz@users.noreply.github.com>
2022-03-28 16:47:18 -0600

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,13 @@
+< html >
+< cuerpo >
+
+Bienvenido <?php  echo  $ _POST [ "nombre" ]; ?> < br >
+Su dirección de nombre es: <?php  echo  $ _POST [ "Nombre" ]; ?> < br >
+<?php  echo  $ _POST [ "Telefono" ]; ?> < br >
+<?php  echo  $ _POST [ "Correo" ]; ?> < br >
+<?php  echo  $ _POST [ "Inscripción" ]; ?> < br >
+
+
+</ cuerpo >
+</ html >
+© 2022 GitHub, Inc.
